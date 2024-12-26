55eb65440fa2e090e656675149704e08df5e4ebf
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2024-12-26 22:07:00 +0000

    refactor:change logic to use router

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 export default function Customers() {
+  const router = useRouter();
   return (
     <div className="w-11/12 max-w-7xl">
       <div className="flex w-full items-center gap-4 justify-between">
         <Input className="w-full max-w-lg" placeholder="Pesquisar..." />
-        <Link href="/customers/new">
-          <Button>Novo Cliente</Button>
-        </Link>
+        <Button onClick={() => router.push("/customers/new")}>
+          Novo Cliente
+        </Button>
       </div>
     </div>
   );
