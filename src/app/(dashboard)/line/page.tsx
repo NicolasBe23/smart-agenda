fc0d7a47f3a00759f17034bddab810cf93726010
Nicolas Bezerra <nicolasbezerra13@gmail.com>
2025-01-13 21:45:35 +0000

    feat: adding new features

--- src/app/(dashboard)/line/page.tsx
+++ src/app/(dashboard)/line/page.tsx
@@ -1,3 +1,27 @@
-export default function Line() {
-  return <div>Line</div>;
+"use client";
+
+import { Customer } from "@/entities/customer";
+import { useDatabase } from "@/database/db";
+
+export default function Customers() {
+  const { customers } = useDatabase();
+
+  return (
+    <div className="w-full flex flex-col gap-4">
+      {customers.map((c) => (
+        <CustomerLine key={c.id} customer={c} />
+      ))}
+    </div>
+  );
+}
+
+function CustomerLine({ customer }: { customer: Customer }) {
+  return (
+    <div className="w-full bg-gray-950 rounded-md p-2 border border-gray-700">
+      <div className="p-2 border-b border-gray-700 flex justify-between items-center">
+        <span>Nome</span>
+        <span>{customer.name}</span>
+      </div>
+    </div>
+  );
 }
